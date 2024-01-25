308ddb5168e93cfb6879b858ad12ffd9081fc322
d-_-b <vcs@ludo.co.za>
2024-01-25 18:06:57 +0200

    wip

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,63 @@
 $(function () {
+    window.app = app = {}
+    // local vars
     inputs = ['category', 'date', 'time', 'location']
+    app.inputs = inputs
     $inputs = {}
-    inputs.forEach(function(input) {
-        $inputs[input] = $('input[name=' + input +']')
+    inputs.forEach(function (input) {
+        $inputs[input] = $('input[name=' + input + ']')
     })
-    $inputs['notes']= $('textarea[name=notes]')
-    $inputs['gmaps']= $('#google-maps')
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        $inputs['location'].val("Geolocation is not supported.");
+    $inputs['notes'] = $('textarea[name=notes]')
+    $inputs['gmaps'] = $('#google-maps')
+    app.$inputs = $inputs
+    db = new PouchDB('lenb')
+    app.db = db
+    app.categories = ['-']
+    function setCategories(cb) {
+        db.get('lenb-categories', function (err, doc) {
+            if (err) {
+                console.log(err);
+                app.categories = ['-']
+                db.put({
+                    _id: 'lenb-categories',
+                    categories: app.categories
+                }, function (err, response) {
+                    if (err) { return console.log(err); }
+                    // handle response
+                   cb()
+                });
+                return
+            }
+            // handle doc
+            app.categories = doc.categories
+            cb()
+        });
     }
 
-    function showError(error) {
-        msg = 'Error'
-        switch (error.code) {
-            case error.PERMISSION_DENIED:
-                msg = "User denied the request for Geolocation."
-                break;
-            case error.POSITION_UNAVAILABLE:
-                msg = "Location information is unavailable."
-                break;
-            case error.TIMEOUT:
-                msg = "The request to get user location timed out."
-                break;
-            case error.UNKNOWN_ERROR:
-                msg = "An unknown error occurred."
-                break;
+    function setLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            $inputs['location'].val("Geolocation is not supported.");
         }
-        $inputs['location'].val(msg);
+        function showError(error) {
+            msg = 'Error'
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    msg = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    msg = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    msg = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    msg = "An unknown error occurred."
+                    break;
+            }
+            $inputs['location'].val(msg);
+        }
     }
 
     function showPosition(position) {
@@ -42,10 +70,30 @@ $(function () {
             window.open(url, "_blank");
         })
     }
-    var date = new Date();
-    var currentDate = date.toISOString().substring(0, 10)
-    var currentTime = date.toISOString().substring(11, 16)
 
-    $inputs['time'].val(currentTime)
-    $inputs['date'].val(currentDate)
+    function addActivity() {
+        var date = new Date();
+        var currentDate = date.toISOString().substring(0, 10)
+        var currentTime = date.toISOString().substring(11, 16)
+        $inputs['time'].val(currentTime)
+        $inputs['date'].val(currentDate)
+        $inputs['notes'].val('')
+        setCategories(function() {
+            $inputs['category'].html('')
+            app.categories.forEach(function (cat) {
+                $inputs['category'].append( $('<option>' + cat + '</option>') )
+            })
+            $inputs['category'].val('-')
+        });
+        
+        setLocation();
+    }
+    function saveActivity() {
+        const data = new FormData(event.target);
+  
+        const formJSON = Object.fromEntries(data.entries());
+    }
+    $('.js-add').click(addActivity)
+    $('.js-save').click(addActivity)
+    addActivity(); // init
 })
---
+++ server.py
@@ -0,0 +1,12 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 8443),
+        SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket (httpd.socket,
+        keyfile="key.pem",
+        certfile='cert.pem', server_side=True)
+
+httpd.serve_forever()
